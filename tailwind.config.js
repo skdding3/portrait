f8ddae9e5fab5488c0e0478599fbc5181b822cfa
skdding3 <skdding3@gmail.com>
2023-04-15 19:55:13 +0900

    font global setting

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,19 @@
 /** @type {import('tailwindcss').Config} */
+
+const { fontFamily } = require('tailwindcss/defaultTheme')
+
+
 module.exports = {
   content: [
     // Or if using `src` directory:
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        mont: ['var(--font-mont)', ...fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 }
