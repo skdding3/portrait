5b8a54f91aa017139f9ac003643d0678821885fd
skdding3 <skdding3@gmail.com>
2023-05-01 15:46:37 +0900

    이력추가

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -8,7 +8,9 @@ const Details = ({position, company, time, address, work}) => {
 
     return <li className="my-8 first:mt-0 last:mb-0 w-[60%] mx-auto flex flex-col  justify-between">
         <LiIcon reference={ref}/>
-        <div>
+        <motion.div initial={{y:50}}
+                    whileInView={{y:0}}
+                    transition={{duration:0.5, type:"spring"}}>
             <h3 className="capitalize font-bold text-2xl">
                 {position}&nbsp;<a className="text-sky-600 capitalize">@{company}</a>
             </h3>
@@ -18,7 +20,7 @@ const Details = ({position, company, time, address, work}) => {
             <p className="font-medium w-full">
                 {work}
             </p>
-        </div>
+        </motion.div>
     </li>
 }
 
@@ -36,13 +38,22 @@ const Experience = () => {
             <h2 className="font-bold text-6xl mb-32 w-full text-center">
                 Experience
             </h2>
-
+            <h3 className="font-bold text-3xl mb-16 w-full text-center">Front-End</h3>
             <div ref={ref} className="w-[60%] relative">
-                <motion.div style={{scaleY: scrollYProgress}} className="absolute left-8 top-0 w-[4px] h-full bg-dark origin-top" />
+                <motion.div style={{scaleY: scrollYProgress}} className="absolute left-9 top-0 w-[4px] h-full bg-dark origin-top" />
                 <ul>
                     <Details position="AI BootCamp" company="Code States" time="2021.05-2021.11" address="Seoul" work="AI 부트캠프 4기를 수료하였습니다." />
                     <Details position="Bachelor of Science" company="Computer Engineering" time="2022.02 ~ " address="" work="컴퓨터 공학사 과정 진행중입니다."  />
                     <Details position="Front-End Engineer" company="Webiznet" time="2022.07-2022.12" address="Seoul" work="솔루션 개발팀에서 프론트엔드 개발을 진행하였습니다."  />
+                </ul>
+            </div>
+            <h3 className="font-bold text-3xl mb-16 w-full text-center">PhotoGrapher</h3>
+            <div ref={ref} className="w-[60%] relative">
+                <motion.div style={{scaleY: scrollYProgress}} className="absolute left-9 top-0 w-[4px] h-full bg-dark origin-top" />
+                <ul>
+                    <Details position="PhotoFreelancer" company="Freelancer" time="" address="" work="MUSINSA, DreamRecord 외 다수" />
+                    <Details position="PhotoGrapher" company="Man's Nonno" time="2017.05-2018.10" address="Japan" work="스트릿 패션 잡지 사진 작업" />
+                    <Details position="PhotoGrapher and Design" company="OMNIVERSE" time="2019.12-2021.03" address="Paju" work="CARIN ,Stonehenge ,Rosemont ,모나코어나더" />
                 </ul>
             </div>
         </div>
--- src/components/LiIcon.js
+++ src/components/LiIcon.js
@@ -11,12 +11,12 @@ const LiIcon = ({reference}) => {
 
     return (
         <figure className="absolute left-0 stroke-dark">
-            <svg width="75" height="75" viewBox="0 0 100 100">
+            <svg className="-rotate-90" width="75" height="75" viewBox="0 0 100 100">
                 <circle cx="75" cy="50" r="20" className="stroke-sky-600 stroke-1 fill-none"/>
                 <motion.circle cx="75" cy="50" r="20" className="stroke-[5px] fill-light"  style={{
                     pathLength: scrollYProgress
                 }}/>
-                <circle cx="75" cy="50" r="10" className="stroke-1 fill-sky-600"/>
+                <circle cx="75" cy="50" r="10" className="animate-pulse stroke-1 fill-sky-600"/>
             </svg>
         </figure>
     );
