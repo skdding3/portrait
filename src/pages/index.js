d7c343d3ffe7d8eb20abe474221ab4be4e7b637f
skdding3 <skdding3@gmail.com>
2023-05-11 18:24:58 +0900

    Resume

--- src/pages/index.js
+++ src/pages/index.js
@@ -33,7 +33,7 @@ export default function Home() {
                                 <Link href="/KIM_SUNKI.pdf" target={"_blank"}
                                       className="flex items-center bg-dark text-light p-2.5 px-6 rounded-lg text-lg font-semibold hover:bg-light hover:text-dark border border-solid border-transparent hover:border-dark dark:bg-light dark:text-dark dark:bg-light dark:text-dark hover:dark:bg-dark hover:dark:border-light hover:dark:text-light md:p-2 md:px-4 md:text-base"
                                 download={true}>
-                                    이력서 <LinkArrow className={"w-5 ml-1"}/>
+                                    Resume <LinkArrow className={"w-5 ml-1"}/>
                                 </Link>
                                 <Link href="about"
                                       className="flex items-center bg-dark text-light p-2.5 px-6 rounded-lg text-lg font-semibold hover:bg-light hover:text-dark border border-solid border-transparent hover:border-dark dark:text-light dark:hover:text-dark md:text-base">
