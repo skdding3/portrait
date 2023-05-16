ca68f331d1da245826bdcc9fe6ebb2678689ced0
skdding3 <skdding3@gmail.com>
2023-05-16 16:18:39 +0900

    사이즈별 반응형 작업

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -8,30 +8,38 @@ import {GithubIcon} from "@/components/Icons";
 import FeatureProjectImg from "../../public/images/projects/Gangnam.png"
 import Project1Img from "../../public/images/projects/grcord.png"
 import Project2Img from "../../public/images/projects/exchange.png"
+import {motion} from "framer-motion";
 
+
+const FramerImage = motion(Image)
 
 const FeaturedProject = ({type, title, summary, img, link, github}) => {
 
     return (
         <article
-            className="w-full flex items-center justify-between rounded-3xl rounded-br-2xl border border-solid border-dark bg-light shadow-2xl relative dark:bg-dark dark:border-light">
-            <div className="absolute top-0 -right-3 -z-10 w-[101%] h-[103%] rounded-[2.5rem] bg-dark rounded-br-3xl dark:bg-light"/>
+            className="w-full flex items-center justify-between rounded-3xl rounded-br-2xl border border-solid border-dark bg-light shadow-2xl relative dark:bg-dark dark:border-light lg:flex-col lg:p-8 xs:rounded-2xl xs:rounded-br-3xl xs:p-4">
+            <div className="absolute top-0 -right-3 -z-10 w-[101%] h-[103%] rounded-[3rem] bg-dark rounded-br-3xl dark:bg-light xs:-right-2 sm:h-[102%] xs:w-full xs:rounded-[1.5rem]"/>
 
-            <Link href={link} target="_blank" className="w-1/2 cursor-pointer overflow-hidden rounded-lg">
-                <Image src={img} alt={title} className="w-full h-auto"/>
+            <Link href={link} target="_blank" className="w-1/2 cursor-pointer overflow-hidden rounded-lg lg:w-full">
+                <FramerImage src={img} alt={title} className="w-full h-auto"
+                             whileHover={{scale:1.05}}
+                             transition={{duration:0.2}}
+                             priority
+                             sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 50vw"
+                />
             </Link>
 
-            <div className="w-1/2 flex flex-col items-start justify-between pl-6">
-                    <span className="text-sky-600 font-medium text-3xl">
+            <div className="w-1/2 flex flex-col items-start justify-between pl-6 lg:w-full lg:pl-0 lg:pt-6">
+                    <span className="text-sky-600 font-medium text-3xl xs:text-base">
                         {type}
                     </span>
-                <h2 className="my-2 w-full text-left text-xl text-bold dark:text-light">{title}</h2>
-                <p className="my-2 font-medium text-dark dark:text-light">
+                <h2 className="my-2 w-full text-left text-xl text-bold dark:text-light sm:text-sm">{title}</h2>
+                <p className="my-2 font-medium text-dark dark:text-light sm:text-sm">
                     {summary}
                 </p>
                 <div className="mt-2 flex items-center">
                     <Link href={github} target="_blank"> </Link>
-                    <Link href={link} target="_blank" className="ml-4 rounded-lg bg-dark text-light p-2 px-6 text-lg font-semibold dark:bg-light dark:text-dark"> 프로젝트 개요 </Link>
+                    <Link href={link} target="_blank" className="ml-4 rounded-lg bg-dark text-light p-2 px-6 text-lg font-semibold dark:bg-light dark:text-dark sm:px-4 sm:text-base"> 프로젝트 개요 </Link>
                 </div>
             </div>
 
@@ -41,23 +49,25 @@ const FeaturedProject = ({type, title, summary, img, link, github}) => {
 
 const Project = ({title, type, img, link, github, summary}) => {
     return (
-        <article className="w-full flex flex-col items-center justify-center rounded-2xl border border-solid border-dark bg-light p-6 relative dark:bg-dark dark:border-light">
-            <div className="absolute top-0 -right-3 -z-10 w-[101%] h-[103%] rounded-[2.5rem] bg-dark rounded-br-3xl dark:bg-light"/>
+        <article className="w-full flex flex-col items-center justify-center rounded-2xl border border-solid border-dark bg-light p-6 relative dark:bg-dark dark:border-light xs:p-4">
+            <div className="absolute top-0 -right-3 -z-10 w-[101%] h-[103%] rounded-[2.5rem] bg-dark rounded-br-3xl dark:bg-light md:-right-2 md:w-[101%] xs:h-[102%]  xs:rounded-[1.5rem]"/>
             <Link href={link} target="_blank" className="w-full cursor-pointer overflow-hidden rounded-lg">
-                <Image src={img} alt={title} className="w-full h-auto"/>
+                <FramerImage src={img} alt={title} className="w-full h-auto"
+                             whileHover={{scale:1.05}}
+                             transition={{duration:0.2}}/>
             </Link>
 
             <div className="w-full flex flex-col items-start justify-between mt-4">
-                    <span className="text-sky-600 font-medium text-3xl">
+                    <span className="text-sky-600 font-medium text-3xl lg:text-lg md:text-base">
                         {type}
                     </span>
-                <h2 className="my-2 w-full text-left text-xl text-bold dark:text-light">{title}</h2>
+                <h2 className="my-2 w-full text-left text-xl text-bold dark:text-light lg:text-2xl">{title}</h2>
                 <p className="my-2 font-medium text-dark dark:text-light">
                     {summary}
                 </p>
                 <div className="w-full mt-2 flex items-center justify-between">
-                    <Link href={link} target="_blank" className="text-lg underline font-semibold dark:text-light"> 프로젝트 개요 </Link>
-                    <Link href={github} target="_blank" className="w-10"> <GithubIcon /> </Link>
+                    <Link href={link} target="_blank" className="text-lg underline font-semibold dark:text-light md:text-base"> 프로젝트 개요 </Link>
+                    <Link href={github} target="_blank" className="w-10 md:w-6"> <GithubIcon /> </Link>
                 </div>
             </div>
         </article>
@@ -75,9 +85,9 @@ const Projects = () => {
             </Head>
             <main className="w-full mb-16 flex flex-col items-center justify-center dark:text-light">
                 <Layout className="pt-16">
-                    <AnimatedText text="Projects" className="mb-16"/>
+                    <AnimatedText text="Projects" className="mb-16 lg:!text-7xl sm:mb-8 sm:!text-6xl xs:!text-4xl"/>
 
-                    <div className="grid grid-cols-12 gap-24 gap-y-32">
+                    <div className="grid grid-cols-12 gap-24 gap-y-32 xl:gap-x-16 lg:gap-x-8 md:gap-y-24 sm:gap-x-0">
                         <div className="col-span-12">
                             <FeaturedProject
                                 title="Webiznet 실서비스 프로젝트"
@@ -91,7 +101,7 @@ const Projects = () => {
                                 img={FeatureProjectImg}
                             />
                         </div>
-                        <div className="col-span-6">
+                        <div className="col-span-6 sm:col-span-12">
                             <Project
                                 title="React 실시간 채팅서비스"
                                 summary="React, Firebase를 활용한 실시간 채팅페이지 서비스입니다."
@@ -101,7 +111,7 @@ const Projects = () => {
                                 img={Project1Img}
                             />
                         </div>
-                        <div className="col-span-6">
+                        <div className="col-span-6 sm:col-span-12">
                             <Project
                                 title="React 환율 계산기 프로젝트"
                                 summary="환율을 계산하는 간단한 프로젝트입니다. 환율 API를 통해 현재 환율을 확인해볼 수 있는 간단한 프로젝트 입니다."
